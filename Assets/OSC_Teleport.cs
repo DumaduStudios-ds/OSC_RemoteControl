46fbf7e380ec75291988073e8090d05272addd4f
arvasil <133312794+arvasil@users.noreply.github.com>
2025-04-07 20:59:26 +0530

    ui and teleport fweature completed

--- Assets/OSC_Teleport.cs
+++ Assets/OSC_Teleport.cs
@@ -4,23 +4,29 @@ using TMPro;
 public class OSC_Teleport : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public string ipAddress = "192.168.0.102";
+    public string ipAddress;
     public int port = 8000;
     private UDPSender oscSender;
     public TextMeshProUGUI inputIp;
     public GameObject ipPage;
     public GameObject teleportPage;
+    [SerializeField] Vector3[] positions, rotations;
+    int heightDisaplcement;
+
     void Start()
     {
-        oscSender = new UDPSender(ipAddress, port);
+        //Debug.Log("ip" + ipAddress);
+        //oscSender = new UDPSender(ipAddress, port);
     }
-    public void Teleport()
+
+    public void Teleport(int num)
     {
-        SendTeleportMessage(new Vector3(100, 100, 100), new Vector3(0,0,0));
+        SendTeleportMessage(positions[num], rotations[num]);
     }
+
     public void SendTeleportMessage(Vector3 position, Vector3 rotation)
     {
-        var message = new OscMessage("/TeleportPlayer", position.x, position.y, position.z);
+        var message = new OscMessage("/TeleportPlayer", position.x, position.y, position.z, rotation.x, rotation.y, rotation.z);
         oscSender.Send(message);
         Debug.Log("OSC Message Sent: " + position.x + ", " + position.y + ", " + position.z);
     }
@@ -32,9 +38,11 @@ public class OSC_Teleport : MonoBehaviour
 
     public void SubmitIP()
     {
-        ipAddress = inputIp.text;
+        ipAddress = inputIp.text.Trim().Replace("\u200B", "").Replace("\u00A0", "");
         ipPage.SetActive(false);
         teleportPage.SetActive(true);
-        
+        Debug.Log("ip  " + ipAddress);
+        oscSender = new UDPSender(ipAddress, port);
     }
+
 }
